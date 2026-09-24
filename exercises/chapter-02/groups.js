1c6f12adec1b6cc4757b091d9024ab04f01274d1
konstantinos.minaidis <konstantinos.minaidis@gmail.com>
2026-09-24 17:00:15 +0300

    Delete exercises directory

--- exercises/chapter-02/groups.js
+++
@@ -1,9 +0,0 @@
-window.GROUPS = window.GROUPS || [];
-window.GROUPS.push(
-  {
-    "id": "chapter-02-7",
-    "chapter": "2",
-    "title": "Καταστατική εξίσωση",
-    "blurb": "Πίεση, θερμοκρασία, ειδικός όγκος — υπολογιστικά θέματα."
-  }
-);
